a09fcb0eef7b74cd57707d22e4d533d52980d79e
Pavel Stepanov <impetus@yandex.ru>
2024-02-12 17:22:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import static com.example.myapplication.MainActivity.KEY;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null) {
+            String str = bundle.getString(KEY);
+        }
+    }
+}
